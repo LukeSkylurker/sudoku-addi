6cbb7c5c912794c5ad05174db1ad099930cd368b
Luke <lucas.arrington@gmail.com>
2026-05-01 17:11:06 -0400

    Add files via upload

---
+++ js/ruby.js
@@ -0,0 +1,242 @@
+// JavaScript source code
+function zeroChangeColor() {
+            if ($("#zero").hasClass("red")) {
+                $("#zero").removeClass("red");
+                $("#zero").addClass("yellow");
+            }
+            else if ($("#zero").hasClass("yellow")) {
+                $("#zero").removeClass("yellow");
+                $("#zero").addClass("blue");
+            }
+            else if ($("#zero").hasClass("blue")) {
+                $("#zero").removeClass("blue");
+                $("#zero").addClass("red");
+            }
+}
+
+function oneChangeColor() {
+
+            if ($("#one").hasClass("red")) {
+                $("#one").removeClass("red");
+                $("#one").addClass("yellow");
+            }
+            else if ($("#one").hasClass("yellow")) {
+                $("#one").removeClass("yellow");
+                $("#one").addClass("blue");
+            }
+            else if ($("#one").hasClass("blue")) {
+                $("#one").removeClass("blue");
+                $("#one").addClass("red");
+            }
+}
+
+function twoChangeColor() {
+
+            if ($("#two").hasClass("red")) {
+                $("#two").removeClass("red");
+                $("#two").addClass("yellow");
+            }
+            else if ($("#two").hasClass("yellow")) {
+                $("#two").removeClass("yellow");
+                $("#two").addClass("blue");
+            }
+            else if ($("#two").hasClass("blue")) {
+                $("#two").removeClass("blue");
+                $("#two").addClass("red");
+            }
+}
+
+function threeChangeColor() {
+            if ($("#three").hasClass("red")) {
+                $("#three").removeClass("red");
+                $("#three").addClass("yellow");
+            }
+            else if ($("#three").hasClass("yellow")) {
+                $("#three").removeClass("yellow");
+                $("#three").addClass("blue");
+            }
+            else if ($("#three").hasClass("blue")) {
+                $("#three").removeClass("blue");
+                $("#three").addClass("red");
+            }
+}
+
+function fourChangeColor() {
+            if ($("#four").hasClass("red")) {
+                $("#four").removeClass("red");
+                $("#four").addClass("yellow");
+            }
+            else if ($("#four").hasClass("yellow")) {
+                $("#four").removeClass("yellow");
+                $("#four").addClass("blue");
+            }
+            else if ($("#four").hasClass("blue")) {
+                $("#four").removeClass("blue");
+                $("#four").addClass("red");
+            }
+}
+
+function fiveChangeColor() {
+            if ($("#five").hasClass("red")) {
+                $("#five").removeClass("red");
+                $("#five").addClass("yellow");
+            }
+            else if ($("#five").hasClass("yellow")) {
+                $("#five").removeClass("yellow");
+                $("#five").addClass("blue");
+            }
+            else if ($("#five").hasClass("blue")) {
+                $("#five").removeClass("blue");
+                $("#five").addClass("red");
+            }
+}
+
+function sixChangeColor() {
+            if ($("#six").hasClass("red")) {
+                $("#six").removeClass("red");
+                $("#six").addClass("yellow");
+            }
+            else if ($("#six").hasClass("yellow")) {
+                $("#six").removeClass("yellow");
+                $("#six").addClass("blue");
+            }
+            else if ($("#six").hasClass("blue")) {
+                $("#six").removeClass("blue");
+                $("#six").addClass("red");
+            }
+}
+
+function sevenChangeColor() {
+            if ($("#seven").hasClass("red")) {
+                $("#seven").removeClass("red");
+                $("#seven").addClass("yellow");
+            }
+            else if ($("#seven").hasClass("yellow")) {
+                $("#seven").removeClass("yellow");
+                $("#seven").addClass("blue");
+            }
+            else if ($("#seven").hasClass("blue")) {
+                $("#seven").removeClass("blue");
+                $("#seven").addClass("red");
+            }
+}
+
+function eightChangeColor() {
+            if ($("#eight").hasClass("red")) {
+                $("#eight").removeClass("red");
+                $("#eight").addClass("yellow");
+            }
+            else if ($("#eight").hasClass("yellow")) {
+                $("#eight").removeClass("yellow");
+                $("#eight").addClass("blue");
+            }
+            else if ($("#eight").hasClass("blue")) {
+                $("#eight").removeClass("blue");
+                $("#eight").addClass("red");
+            }
+}
+
+function nineChangeColor() {
+            if ($("#nine").hasClass("red")) {
+                $("#nine").removeClass("red");
+                $("#nine").addClass("yellow");
+            }
+            else if ($("#nine").hasClass("yellow")) {
+                $("#nine").removeClass("yellow");
+                $("#nine").addClass("blue");
+            }
+            else if ($("#nine").hasClass("blue")) {
+                $("#nine").removeClass("blue");
+                $("#nine").addClass("red");
+            }
+}
+
+function tenChangeColor() {
+            if ($("#ten").hasClass("red")) {
+                $("#ten").removeClass("red");
+                $("#ten").addClass("yellow");
+            }
+            else if ($("#ten").hasClass("yellow")) {
+                $("#ten").removeClass("yellow");
+                $("#ten").addClass("blue");
+            }
+            else if ($("#ten").hasClass("blue")) {
+                $("#ten").removeClass("blue");
+                $("#ten").addClass("red");
+            }
+}
+
+function elevenChangeColor() {
+            if ($("#eleven").hasClass("red")) {
+                $("#eleven").removeClass("red");
+                $("#eleven").addClass("yellow");
+            }
+            else if ($("#eleven").hasClass("yellow")) {
+                $("#eleven").removeClass("yellow");
+                $("#eleven").addClass("blue");
+            }
+            else if ($("#eleven").hasClass("blue")) {
+                $("#eleven").removeClass("blue");
+                $("#eleven").addClass("red");
+            }
+}
+
+function twelveChangeColor() {
+            if ($("#twelve").hasClass("red")) {
+                $("#twelve").removeClass("red");
+                $("#twelve").addClass("yellow");
+            }
+            else if ($("#twelve").hasClass("yellow")) {
+                $("#twelve").removeClass("yellow");
+                $("#twelve").addClass("blue");
+            }
+            else if ($("#twelve").hasClass("blue")) {
+                $("#twelve").removeClass("blue");
+                $("#twelve").addClass("red");
+            }
+}
+
+function thirteenChangeColor() {
+            if ($("#thirteen").hasClass("red")) {
+                $("#thirteen").removeClass("red");
+                $("#thirteen").addClass("yellow");
+            }
+            else if ($("#thirteen").hasClass("yellow")) {
+                $("#thirteen").removeClass("yellow");
+                $("#thirteen").addClass("blue");
+            }
+            else if ($("#thirteen").hasClass("blue")) {
+                $("#thirteen").removeClass("blue");
+                $("#thirteen").addClass("red");
+            }
+}
+
+function fourteenChangeColor() {
+            if ($("#fourteen").hasClass("red")) {
+                $("#fourteen").removeClass("red");
+                $("#fourteen").addClass("yellow");
+            }
+            else if ($("#fourteen").hasClass("yellow")) {
+                $("#fourteen").removeClass("yellow");
+                $("#fourteen").addClass("blue");
+            }
+            else if ($("#fourteen").hasClass("blue")) {
+                $("#fourteen").removeClass("blue");
+                $("#fourteen").addClass("red");
+            }
+}
+
+function fifteenChangeColor() {
+            if ($("#fifteen").hasClass("red")) {
+                $("#fifteen").removeClass("red");
+                $("#fifteen").addClass("yellow");
+            }
+            else if ($("#fifteen").hasClass("yellow")) {
+                $("#fifteen").removeClass("yellow");
+                $("#fifteen").addClass("blue");
+            }
+            else if ($("#fifteen").hasClass("blue")) {
+                $("#fifteen").removeClass("blue");
+                $("#fifteen").addClass("red");
+            }
+}
